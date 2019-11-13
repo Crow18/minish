85eafb8fd29e75856eacb5612ae80f28ce970aa5
Carl <carlligumaofficial@gmail.com>
2019-11-13 18:27:04 +0200

    eh

--- read.c
+++ read.c
@@ -1,7 +1,6 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
-
 
 int lenstr(char *str)
 {
@@ -15,14 +14,13 @@ int lenstr(char *str)
     return (i);
 }
 
-char *getstr()
+char *getstr(void)
 {
     char *strn;
     int i;
     char str[400];
-    read(0, str, 400);
 
-    
+    read(0, str, 400); 
     strn = (char*)malloc((lenstr(str)) * sizeof(char)+ 1);
     while(str[i])
     {
@@ -35,8 +33,13 @@ char *getstr()
         i++;
     }
     strn[i] = '\0';
+    i = 0;
     return(strn);
+}
 
+void ftfre(char *str)
+{
+    free(str);
 }
 
 void putstr(char *strg)
@@ -49,12 +52,45 @@ void putstr(char *strg)
     }
 }
 
+int splitter()
+{
+    char str[100];
+    char splitStrings[400][400]; //can store 10 words of 10 characters
+    int i,j,cnt;
+ 
+    printf("Enter a string: ");
+    gets(str);
+ 
+    j=0; cnt=0;
+    for(i=0;i<=(strlen(str));i++)
+    {
+        // if space or NULL found, assign NULL into splitStrings[cnt]
+        if(str[i] == ' ' || str[i]=='\0')
+        {
+            splitStrings[cnt][j]='\0';
+            cnt++;  //for next word
+            j=0;    //for next word, init index to 0
+        }
+        else
+        {
+            splitStrings[cnt][j]=str[i];
+            j++;
+        }
+    }
+    printf("\nOriginal String is: %s",str);
+    printf("\nStrings (words) after split by space:\n");
+    for(i=0;i < cnt;i++)
+        printf("%s\n",splitStrings[i]);
+    return 0;
+}
+
 main()
 {
-    // printf("%d\n", lenstr(getstr()));
-    // printf("%s", getstr());
-    // printf("%s", getstr());
     putstr(getstr());
-    // putstr(getstr());
-    // putstr(getstr());
+    putstr("\n");
+    putstr(getstr());
+    putstr("\n");
+    putstr(getstr());
+    putstr("\n");
 }
+
