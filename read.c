880262e53f8ac9f2912af4c9bb9bc5586256aaa5
Carl LIGUMA <cliguma@c3r3s1.wethinkcode.co.za>
2019-11-18 09:27:45 +0200

    normsomewhat

--- read.c
+++ read.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-int lenstr(char *str)
+#define NUM 400;
+
+int		lenstr(char *str)
 {
-    int i;
+	int		i;
 
     i = 0;
     while(str[i] != '\0')
@@ -14,15 +15,18 @@ int lenstr(char *str)
     return (i);
 }
 
-char *getstr(void)
+char	*getstr(void)
 {
     char *strn;
     int i;
-    char str[400];
+	int n;
 
-    read(0, str, 400); 
+	n = NUM;
+    char str[n];
+
+    read(0, str, n); 
     strn = (char*)malloc((lenstr(str)) * sizeof(char)+ 1);
-    while(str[i])
+    while (str[i])
     {
         strn[i]=str[i];
         
@@ -37,12 +41,8 @@ char *getstr(void)
     return(strn);
 }
 
-void ftfre(char *str)
-{
-    free(str);
-}
 
-void putstr(char *strg)
+void	putstr(char *strg)
 {
     int i = 0;
     while(strg[i])
@@ -52,45 +52,8 @@ void putstr(char *strg)
     }
 }
 
-int splitter()
-{
-    char str[100];
-    char splitStrings[400][400]; //can store 10 words of 10 characters
-    int i,j,cnt;
- 
-    printf("Enter a string: ");
-    gets(str);
- 
-    j=0; cnt=0;
-    for(i=0;i<=(strlen(str));i++)
-    {
-        // if space or NULL found, assign NULL into splitStrings[cnt]
-        if(str[i] == ' ' || str[i]=='\0')
-        {
-            splitStrings[cnt][j]='\0';
-            cnt++;  //for next word
-            j=0;    //for next word, init index to 0
-        }
-        else
-        {
-            splitStrings[cnt][j]=str[i];
-            j++;
-        }
-    }
-    printf("\nOriginal String is: %s",str);
-    printf("\nStrings (words) after split by space:\n");
-    for(i=0;i < cnt;i++)
-        printf("%s\n",splitStrings[i]);
-    return 0;
-}
-
-main()
+int		main(void)
 {
     putstr(getstr());
     putstr("\n");
-    putstr(getstr());
-    putstr("\n");
-    putstr(getstr());
-    putstr("\n");
 }
-
